c5803c1b9d590db0acc86046e2107a097b0dd3b8
n4ch3t3 <36861802+n4ch3t3@users.noreply.github.com>
2022-12-05 20:27:26 +0100

    part 2 initial solution

--- day_02_rock_paper_scissors/day02.py
+++ day_02_rock_paper_scissors/day02.py
@@ -1,10 +1,10 @@
 Scores = {
-    'A': 1,
-    'B': 2,
-    'C': 3,
-    'X': 1,
-    'Y': 2,
-    'Z': 3,
+    'A': 1, #rock
+    'B': 2, #paper
+    'C': 3, #scissors
+    'X': 1, #rock, lose
+    'Y': 2, #paper, draw
+    'Z': 3, #scissors, win
     'AX': 3,
     'AY': 6,
     'AZ': 0,
@@ -16,13 +16,37 @@ Scores = {
     'CZ': 3
 }
 
+Scores2 = {
+    'X': 0,  # lose
+    'Y': 3,  # draw
+    'Z': 6 # win
+}
+
+decision = {}
+
+decision[('A', 'X')] = 'C'
+decision[('A', 'Y')] = 'A'
+decision[('A', 'Z')] = 'B'
+decision[('B', 'X')] = 'A'
+decision[('B', 'Y')] = 'B'
+decision[('B', 'Z')] = 'C'
+decision[('C', 'X')] = 'B'
+decision[('C', 'Y')] = 'C'
+decision[('C', 'Z')] = 'A'
+
+# X lose, Y draw, Z win
+
 with open('input.txt') as f:
     lines = f.readlines()
 
-score = 0
+score_1 = 0
+score_2 = 0
 
 for line in lines:
     values = line.rstrip().split(' ')
-    score = score + Scores[values[1]] + Scores[''.join(values)]
+    score_1 = score_1 + Scores[values[1]] + Scores[''.join(values)]
+    score_2 = score_2 + Scores[decision[(values[0], values[1])]] + Scores2[values[1]]
 
-print(score)
+print("Part 1:", score_1)
+print("Part 2:", score_2)
+

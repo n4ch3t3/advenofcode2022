c5891880a9696f6bc4f2dbc5b0a6f19c02c99dce
n4ch3t3 <36861802+n4ch3t3@users.noreply.github.com>
2022-12-28 22:13:20 +0100

    better version

--- day_03_rucksack_reorganization/day03.py
+++ day_03_rucksack_reorganization/day03.py
@@ -1,29 +1,16 @@
 import string
 
-letters = string.ascii_letters
-priority = {}
-priority_value = 1
-for letter in letters:
-    priority[letter] = priority_value
-    priority_value += 1
-
-repeated_items = []
+def misplaced_item(rucksack: str) -> str:
+    half = len(rucksack) // 2
+    [letter] = set(rucksack[:half]).intersection(rucksack[half:])
+    return letter
 
 with open('/home/h4x0r/projects/adventofcode2022/day_03_rucksack_reorganization/input.txt') as f:
     rucksacks = [line.rstrip() for line in f]
 
-for list_of_items in rucksacks:
-    number_of_items = len(list_of_items)
-    if number_of_items % 2 == 0:
-        first_compartment = list_of_items[0:number_of_items//2]
-        second_compartment = list_of_items[number_of_items//2:]
-    else:
-        first_compartment = list_of_items[0:number_of_items//2+1]
-        second_compartment = list_of_items[number_of_items//2+1:]
-    repeated = set(first_compartment).intersection(second_compartment)
-    repeated_items.extend(repeated)
+priorities = {letter: i for i, letter in enumerate(string.ascii_letters, 1)}
 
-priority_sum = sum(priority[letter] for letter in repeated_items)
+priority_sum = sum(priorities[misplaced_item(r)] for r in rucksacks)
 
 print(f'Priority sum: {priority_sum}')
 

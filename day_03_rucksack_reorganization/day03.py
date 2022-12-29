688e627e51059641ed470901d0b7b72b8450a470
n4ch3t3 <36861802+n4ch3t3@users.noreply.github.com>
2022-12-30 00:54:18 +0100

    part 2 better version

--- day_03_rucksack_reorganization/day03.py
+++ day_03_rucksack_reorganization/day03.py
@@ -1,16 +1,29 @@
+from typing import Iterator
 import string
+
 
 def misplaced_item(rucksack: str) -> str:
     half = len(rucksack) // 2
     [letter] = set(rucksack[:half]).intersection(rucksack[half:])
     return letter
 
+
+def badge(rucksack: str, *rucksacks: str) -> str:
+    [letter] = set(rucksack).intersection(*rucksacks)
+    return letter
+
+
+def badges(*rucksacks: str) -> Iterator[str]:
+    it = iter(rucksacks)
+    for group in zip(it, it, it):
+        yield badge(*group)
+
+
 with open('/home/h4x0r/projects/adventofcode2022/day_03_rucksack_reorganization/input.txt') as f:
     rucksacks = [line.rstrip() for line in f]
 
 priorities = {letter: i for i, letter in enumerate(string.ascii_letters, 1)}
 
-priority_sum = sum(priorities[misplaced_item(r)] for r in rucksacks)
+print("Part 1:", sum(priorities[misplaced_item(r)] for r in rucksacks))
 
-print(f'Priority sum: {priority_sum}')
-
+print("Part 2:", sum(priorities[badge] for badge in badges(*rucksacks)))
